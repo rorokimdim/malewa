73b36c5281d15503f5b7b6d47b0fd26d78c3848d
Amit Shrestha <ashrestha@housecanary.com>
2017-05-18 10:13:22 -0700

    Lower max permissible retirement year.

--- src/malewa/dao.cljs
+++ src/malewa/dao.cljs
@@ -3,7 +3,7 @@
             [alandipert.storage-atom :as ls]
             [malewa.utils :as u]))
 
-(def MAX-VALID-TARGET-RETIREMENT-YEAR 100)
+(def MAX-VALID-TARGET-RETIREMENT-YEAR 80)
 
 (def APP-STATE
   (ls/local-storage
