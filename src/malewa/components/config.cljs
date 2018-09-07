8ea89fb2872e6e73dccd56a8eaa5688f2a06221c
Amit Shrestha <ashrestha@housecanary.com>
2018-09-07 14:21:57 -0700

    Fix config ui slowness.

--- src/malewa/components/config.cljs
+++ src/malewa/components/config.cljs
@@ -35,7 +35,7 @@
   [:input {:type "text"
            :id key
            :style {:text-align "right"}
-           :value (key (get-config))
+           :default-value (key (get-config))
            :on-change (partial update-config-on-change key)}])
 
 (defn config-slider-input-comp
